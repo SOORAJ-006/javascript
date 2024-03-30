158b9c680d3ed563d88cdcb1fa9e2f7da73a325d
SOORAJ-006 <lsoorajlal5git@gmail.com>
2024-03-30 14:15:58 +0530

    v6

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ function display(id){
     document.getElementById('content').style.filter = "blur(3px)" ;
 }
 
+let s = 0;
+
 let form = document.getElementById('form');
 form.addEventListener('submit' , function(event){
     event.preventDefault();
@@ -26,58 +28,40 @@ form.addEventListener('submit' , function(event){
     let state = document.getElementById('state').value;
     let city = document.getElementById('city').value;
     let pin = document.getElementById('pin').value;
-    let gender = document.getElementsByClassName('gender').value;
+    let gender = document.querySelector('input[name="inlineRadioOptions"]:checked').value;
 
-    // console.log(fName);
-    // console.log(lName);
-    // console.log(email);
-    // console.log(mob);
-    // console.log(dob);
-    // console.log(address);
-    // console.log(country);
-    // console.log(state);
-    // console.log(city);
-    // console.log(pin);
-    // console.log(salutation);
-    // console.log(gender);
+    // showing data in the home page
 
     let Namee = salutation + ' ' +  fName +  ' ' + lName
         console.log(Namee);
     
-    document.getElementById('Name').innerHTML = Namee;
-    let Email = document.getElementById('email');
-    let Mob = document.getElementById('mob');
-    let Dob = document.getElementById('dob');
-    let Country = document.getElementById('country');
+    // document.getElementById('Name').innerHTML = Namee;
+    // let Email = document.getElementById('email');
+    // let Mob = document.getElementById('mob');
+    // let Dob = document.getElementById('dob');
+    // let Country = document.getElementById('country');
+
+    let tr = document.createElement('tr');
+    let tds = tr.appendChild(document.createElement('td'));
+    let td1 = tr.appendChild(document.createElement('td'));
+    let td2 = tr.appendChild(document.createElement('td'));
+    let td3 = tr.appendChild(document.createElement('td'));
+    let td4 = tr.appendChild(document.createElement('td'));
+    let td5 = tr.appendChild(document.createElement('td'));
+    let td6 = tr.appendChild(document.createElement('td'));
+
+
+
+    tds.innerHTML = ++s;
+    td1.innerHTML = Namee;
+    td2.innerHTML = email;
+    td3.innerHTML = mob;
+    td4.innerHTML = gender;
+    td5.innerHTML = dob;
+    td6.innerHTML = country;
+
+    document.getElementById('tbl').appendChild(tr);
 
 })
 
 
-// function submit()
-// {
-//     submit.preventDefault();
-
-//     let salutation = document.getElementById('salutation').value;
-//     let fName = document.getElementById('fName').value;
-//     let lName = document.getElementById('lName').value;
-//     let email = document.getElementById('email').value;
-//     let mob = document.getElementById('mob').value;
-//     let dob = document.getElementById('dob').value;
-//     let address = document.getElementById('address').value;
-//     let country = document.getElementById('country').value;
-//     let state = document.getElementById('state').value;
-//     let city = document.getElementById('city').value;
-//     let pin = document.getElementById('pin').value;
-//     let gender = document.getElementsByClassName('gender').value;
-
-//     let Namee = salutation + ' ' +  fName +  ' ' + lName
-//         console.log(Namee);
-    
-//     let Name = document.getElementById('Name').innerHTML;
-//     let Email = document.getElementById('email');
-//     let Mob = document.getElementById('mob');
-//     let Dob = document.getElementById('dob');
-//     let Country = document.getElementById('country');
-
-//     Name = Namee;
-// }
