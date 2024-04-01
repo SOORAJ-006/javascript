23e018dec7549caa0f9011ec298281765cec4a1a
SOORAJ-006 <lsoorajlal5git@gmail.com>
2024-04-01 13:56:30 +0530

    j1

--- index.js
+++ index.js
@@ -11,57 +11,63 @@ function display(id){
     document.getElementById('content').style.filter = "blur(3px)" ;
 }
 
-let s = 0;
+// let s = 0;
 
-let form = document.getElementById('form');
-form.addEventListener('submit' , function(event){
-    event.preventDefault();
+// let form = document.getElementById('form');
+// form.addEventListener('submit' , function(event){
+//     event.preventDefault();
 
-    let salutation = document.getElementById('salutation').value;
-    let fName = document.getElementById('fName').value;
-    let lName = document.getElementById('lName').value;
-    let email = document.getElementById('email').value;
-    let mob = document.getElementById('mob').value;
-    let dob = document.getElementById('dob').value;
-    let address = document.getElementById('address').value;
-    let country = document.getElementById('country').value;
-    let state = document.getElementById('state').value;
-    let city = document.getElementById('city').value;
-    let pin = document.getElementById('pin').value;
-    let gender = document.querySelector('input[name="inlineRadioOptions"]:checked').value;
+//     let salutation = document.getElementById('salutation').value;
+//     let fName = document.getElementById('fName').value;
+//     let lName = document.getElementById('lName').value;
+//     let email = document.getElementById('email').value;
+//     let mob = document.getElementById('mob').value;
+//     let dob = document.getElementById('dob').value;
+//     let address = document.getElementById('address').value;
+//     let country = document.getElementById('country').value;
+//     let state = document.getElementById('state').value;
+//     let city = document.getElementById('city').value;
+//     let pin = document.getElementById('pin').value;
+//     let gender = document.querySelector('input[name="inlineRadioOptions"]:checked').value;
 
-    // showing data in the home page
+//     // showing data in the home page
 
-    let Namee = salutation + ' ' +  fName +  ' ' + lName
-        console.log(Namee);
+//     let Namee = salutation + ' ' +  fName +  ' ' + lName
+//         console.log(Namee);
     
-    // document.getElementById('Name').innerHTML = Namee;
-    // let Email = document.getElementById('email');
-    // let Mob = document.getElementById('mob');
-    // let Dob = document.getElementById('dob');
-    // let Country = document.getElementById('country');
+//     // document.getElementById('Name').innerHTML = Namee;
+//     // let Email = document.getElementById('email');
+//     // let Mob = document.getElementById('mob');
+//     // let Dob = document.getElementById('dob');
+//     // let Country = document.getElementById('country');
 
-    let tr = document.createElement('tr');
-    let tds = tr.appendChild(document.createElement('td'));
-    let td1 = tr.appendChild(document.createElement('td'));
-    let td2 = tr.appendChild(document.createElement('td'));
-    let td3 = tr.appendChild(document.createElement('td'));
-    let td4 = tr.appendChild(document.createElement('td'));
-    let td5 = tr.appendChild(document.createElement('td'));
-    let td6 = tr.appendChild(document.createElement('td'));
+//     let tr = document.createElement('tr');
+//     let tds = tr.appendChild(document.createElement('td'));
+//     let td1 = tr.appendChild(document.createElement('td'));
+//     let td2 = tr.appendChild(document.createElement('td'));
+//     let td3 = tr.appendChild(document.createElement('td'));
+//     let td4 = tr.appendChild(document.createElement('td'));
+//     let td5 = tr.appendChild(document.createElement('td'));
+//     let td6 = tr.appendChild(document.createElement('td'));
+//     let td7 = tr.appendChild(document.createElement('td'));
 
 
 
-    tds.innerHTML = ++s;
-    td1.innerHTML = Namee;
-    td2.innerHTML = email;
-    td3.innerHTML = mob;
-    td4.innerHTML = gender;
-    td5.innerHTML = dob;
-    td6.innerHTML = country;
+//     tds.innerHTML = ++s;
+//     td1.innerHTML = Namee;
+//     td2.innerHTML = email;
+//     td3.innerHTML = mob;
+//     td4.innerHTML = gender;
+//     td5.innerHTML = dob;
+//     td6.innerHTML = country;
+//     //  
+//     document.getElementById('tbl').appendChild(tr);
+    
+// })
 
-    document.getElementById('tbl').appendChild(tr);
 
+
+
+fetch("http://localhost:3000/employees").then((data) =>{
+    console.log(data);
 })
-
-
