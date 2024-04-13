b5c2fd44440fe1bbdaf7c53ab9f7c88336c8195f
SOORAJ-006 <lsoorajlal5git@gmail.com>
2024-04-13 14:20:12 +0530

    j7

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ fetchData();
 // fetching data from the json
 function fetchData(){
 
-fetch('http://localhost:3000/employees').then((data) => {
+    var page = 1;
+    var postPerPage = 5;
+    var skip = (page - 1) * postPerPage;
+
+    fetch(`http://localhost:3000/employees/`).then((data) => {
     // console.log(data);
     return data.json();
 }).then((objectData) => {
@@ -36,7 +40,7 @@ console.log(objectData);
 
         tableData+= `<tr>
         <th id="id">#${++sl}</th>
-        <td id="Name">${values.salutation} ${values.firstName} ${values.lastName}</td>
+        <td id="Name"><img class="emp-img" src="http://localhost:3000/employees/${values.id}/avatar"> ${values.salutation} ${values.firstName} ${values.lastName}</td>
         <td id="Email">${values.email}</td>
         <td id="Mob">${values.phone}</td>
         <td id="Gender">${values.gender}</td>
@@ -51,8 +55,11 @@ console.log(objectData);
               <li onclick="editEmployee('${values.id}')"><a class="dropdown-item" href="#">Edit </a></li>
               <li onclick="deleteEmployee('${values.id}')"><a class="dropdown-item" href="#">Delete</a></li>
             </ul>
-          </div></td>
+          </div>
+        </td>
     </tr>`
+
+    console.log("table"+"  " +objectData.length);
 
     });
 
@@ -60,11 +67,155 @@ console.log(objectData);
 })
 }
 
+// ---------------------------Add Form Validation-------------------------------
 
+function addFormValidation(){
+  const salutation = document.getElementById("addSalutation").value.trim(); 
+    const firstName = document.getElementById("addFirstName").value.trim();
+    const lastName = document.getElementById("addLastName").value.trim();
+    const email = document.getElementById("addEmail").value.trim();
+    const phone = document.getElementById("addPhone").value.trim();
+    
+    const address = document.getElementById("addAddress").value.trim();
+    const country = document.getElementById("addCountry").value.trim();
+    const state = document.getElementById("addState").value.trim();
+    const city = document.getElementById("addCity").value.trim();
+    const pin = document.getElementById("addPin").value.trim();
+    const username = document.getElementById("adduserName").value.trim();
+    const password = document.getElementById("addPassword").value.trim();
+    const qualifications = document.getElementById("addqualification").value.trim();
+
+  // DOB
+
+  const dob = document.getElementById("addDob")
+  const addDovValidation = document.getElementById('addDobError')
+  const dobvalue = dob.value.trim();
+
+  const gender = document.querySelector('input[name="gender"]:checked')
+  const addGenderValidation = document.getElementById('errorGender')
+
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  const phonePattern = /^\d{10}$/
+  const namePattern = /^[A-Za-z]+$/
+
+  let isValid = true;
+
+  // validating DOB and Gender
+
+  if(gender){
+    addGenderValidation.textContent = ""
+
+  }
+  else{
+    addGenderValidation.textContent = "* please select gender"
+    isValid = false
+  }
+
+  if(dobvalue === ""){
+    addDovValidation.textContent = "* please select Date of Birth"
+    isValid = false
+  }
+
+  // validating rest
+
+  if(!phonePattern.test(phone)){
+    document.getElementById('addPhoneError').textContent = "* phone number should contain 10n digits"
+    isValid = false
+  }
+
+  if(!emailPattern.test(email)){
+    document.getElementById('addEmailError').textContent = "* Invalid email"
+    isValid = false
+  }
+
+  if(!namePattern.test(firstName)){
+    document.getElementById('addFirstNameError').textContent = "* please enter first name"
+    isValid = false
+  }
+
+  if(!namePattern.test(lastName)){
+    document.getElementById('addLastNameError').textContent = "* please enter first name"
+    isValid = false
+  }
+
+  if(password == ""){
+    document.getElementById('addPasswordError').textContent = "* please enter password"
+    isValid = false
+  }
+
+  if(salutation == "" || salutation == "select"){
+    document.getElementById('addSalutationError').textContent = "* saluration is needed"
+  }
+
+  if(username == ""){
+    document.getElementById('adduserNameError').textContent = "* username is needed"
+  }
+
+  if(address == ""){
+    document.getElementById('addAddressError').textContent = "* address is needed"
+  }
+
+  if(qualifications == ""){
+    document.getElementById('addqualificationError').textContent = "* qualification is needed"
+  }
+
+  if(country == "" || country == "select"){
+    document.getElementById('addCountryError').textContent = "* country is needed"
+  }
+
+  if(state == "" || state == "select"){
+    document.getElementById('addStateError').textContent = "* state is needed"
+  }
+
+  if(city == "" || city == "select"){
+    document.getElementById('addCityError').textContent = "* city is needed"
+  }
+
+  if(pin == ""){
+    document.getElementById('addPinError').textContent = "* pin is needed"
+  }
+
+  // validation text event
+
+  document.getElementById('addEmployee').addEventListener('input' , (event) =>{
+    inputId = event.target.id;
+    const errorId = `${inputId}Error`;
+    console.log("error id is ", errorId);
+    document.getElementById(errorId).textContent = "";  
+  })
+
+  // gender validation
+
+  const male = document.getElementById("forMale")
+  const female = document.getElementById("forFemale")
+
+  male.addEventListener("click" , () =>{
+    document.getElementById("errorGender").textContent = "";
+  })
+
+  female.addEventListener("click" , () =>{
+    document.getElementById("errorGender").textContent = "";
+  })
+
+  return isValid;
+}
+
+// ------------------------------ADD EMPLOYEE ---------------------------------
+
+const addEmployeeSubmit = document.getElementById('addEmployeeBtn');
+addEmployeeSubmit.addEventListener('click' , () =>{
+  const validation = addFormValidation();
+  console.log(validation);
+  if(!validation){
+    return;
+  }else{
+    addEmpsubmit()
+  }
+})
 
 function addEmpsubmit() {
-    
-    const salutation = document.getElementById("addSalutation").value;
+  
+    const salutation = document.getElementById("addSalutation").value; 
     const firstName = document.getElementById("addFirstName").value;
     const lastName = document.getElementById("addLastName").value;
     const email = document.getElementById("addEmail").value;
@@ -92,34 +243,32 @@ function addEmpsubmit() {
     console.log(reversedDateString);
     const dobb = reversedDateString;
 
- const newData = {
-    salutation,
-    firstName,
-    lastName,
-    email,
-    phone,
-    dob:dobb,
-    gender,
-    qualifications,
-    address,
-    city,
-    state,
-    pin,
-    country,
-    username,
-    password
-      
- }
-
- console.log(newData);
-
-
+      const newData = {
+        salutation,
+        firstName,
+        lastName,
+        email,
+        phone,
+        dob:dobb,
+        gender,
+        qualifications,
+        address,
+        city,
+        state,
+        pin,
+        country,
+        username,
+        password       
+    }
+ console.log("the data we need" + newData.address);
  postData(newData);
     
  }
+// }
 
-//  post data  
 
+
+//  post data -------------------------- 
 
 function postData(newData) {
     fetch('http://localhost:3000/employees', {
@@ -131,19 +280,49 @@ function postData(newData) {
     })
     .then(response => {
         return response.json();
+
+        
     })
     .then(data => {
         console.log("Data posted successfully:", data);
         // Additional actions if needed after successful posting
+
+        // getting id of the created employee
+
+        var user = data.id;
+        newData.id = user;
+        console.log(newData);
+
+        // image upload-------------------------------------
+
+        const profileImg = document.getElementById('img-upload');
+        var imgObject = new FormData();
+        imgObject.append("avatar", profileImg.files[0]);
+        console.log("img added succesfully" , imgObject);
+
+        fetch(`http://localhost:3000/employees/${data.id}/avatar`,{
+            method: "POST",
+            body: imgObject,
+        });
+        // tableContents.unshift(newData);
+        console.log(newData);
+        // displayData(currentPage);
+
     })
+    .then(() =>{
+        swal.fire({
+            icon: "success",
+            title: "ADD EMPLOYEE SUCCESSFULL",
+            showConfirmButton: false,
+            timer: 1500,
+        });
+    });
     Close('addEmployee');
 }
 
 
-// edit employee
-
-
 //  edit employee Get
+
 
 function editEmployee(empid){
 
@@ -165,6 +344,11 @@ fetch(`http://localhost:3000/employees/${empid}` , {
     .then((response) => response.json())
     .then((data) => {
         console.log(data); //displaying all the data
+
+
+        // var image = document.getElementById('img-upload').src
+        // image = `http://localhost:3000/employees/${data.empid}/avatar`;
+
 
         document.getElementById('editSalutation').value = data.salutation;
         document.getElementById('editFirstName').value = data.firstName;
@@ -193,16 +377,171 @@ fetch(`http://localhost:3000/employees/${empid}` , {
         // const gender = document.querySelector('input[name="gender"]:checked').value;
     })
 
+    // image preview in edit employee form
+
+    const editpreview = document.getElementById('image-box')
+    editpreview.src = `http://localhost:3000/employees/${empid}/avatar`;
+
+
     let editsubmit = document.getElementById("saveEdit");
     editsubmit.addEventListener("click",() =>{
-        saveChanges(empid);
+      const validation = EditFormValidation();
+      console.log(validation);
+  if(!validation){
+    return;
+  }else{
+    saveChanges(empid);
+  }
+        
     }) 
+   
+}
+
+function avatarPreview(){
+    const preview = document.getElementById("image-box");
+    preview.src = URL.createObjectURL(event.target.files[0]);
+
+}
+
+
+// Edit Employee Validation
+
+function EditFormValidation(){
+  const salutation = document.getElementById("editSalutation").value.trim(); 
+    const firstName = document.getElementById("editFirstName").value.trim();
+    const lastName = document.getElementById("editLastName").value.trim();
+    const email = document.getElementById("editEmail").value.trim();
+    const phone = document.getElementById("editPhone").value.trim();
+    const address = document.getElementById("editAddress").value.trim();
+    const country = document.getElementById("editCountry").value.trim();
+    const state = document.getElementById("editState").value.trim();
+    const city = document.getElementById("editCity").value.trim();
+    const pin = document.getElementById("editPin").value.trim();
+    const username = document.getElementById("editUserName").value.trim();
+    const password = document.getElementById("editPassword").value.trim();
+    const qualifications = document.getElementById("editQualification").value.trim();
+
+  // DOB
+
+  const dob = document.getElementById("editDob")
+  const addDovValidation = document.getElementById('editDobError')
+  const dobvalue = dob.value.trim();
+
+  const gender = document.querySelector('input[name="editGender"]:checked')
+  const addGenderValidation = document.getElementById('editGenderError')
+
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  const phonePattern = /^\d{10}$/
+  const namePattern = /^[A-Za-z]+$/
+
+  let isValid = true;
+
+  // validating DOB and Gender
+
+  if(gender){
+    addGenderValidation.textContent = ""
+
+  }
+  else{
+    addGenderValidation.textContent = "* please select gender"
+    isValid = false
+  }
+
+  if(dobvalue === ""){
+    addDovValidation.textContent = "* please select Date of Birth"
+    isValid = false
+  }
+
+  // validating rest
+
+  if(!phonePattern.test(phone)){
+    document.getElementById('editPhoneError').textContent = "* phone number should contain 10n digits"
+    isValid = false
+  }
+
+  if(!emailPattern.test(email)){
+    document.getElementById('editEmailError').textContent = "* Invalid email"
+    isValid = false
+  }
+
+  if(!namePattern.test(firstName)){
+    document.getElementById('editFirstNameError').textContent = "* please enter first name"
+    isValid = false
+  }
+
+  if(!namePattern.test(lastName)){
+    document.getElementById('editLastNameError').textContent = "* please enter first name"
+    isValid = false
+  }
+
+  if(password == ""){
+    document.getElementById('editPasswordError').textContent = "* please enter password"
+    isValid = false
+  }
+
+  if(salutation == "" || salutation == "select"){
+    document.getElementById('editSalutationError').textContent = "* saluration is needed"
+  }
+
+  if(username == ""){
+    document.getElementById('editUserNameError').textContent = "* username is needed"
+  }
+
+  if(address == ""){
+    document.getElementById('editAddressError').textContent = "* address is needed"
+  }
+
+  if(qualifications == ""){
+    document.getElementById('editQualificationError').textContent = "* qualification is needed"
+  }
+
+  if(country == "" || country == "select"){
+    document.getElementById('editCountryError').textContent = "* country is needed"
+  }
+
+  if(state == "" || state == "select"){
+    document.getElementById('editStateError').textContent = "* state is needed"
+  }
+
+  if(city == "" || city == "select"){
+    document.getElementById('editCityError').textContent = "* city is needed"
+  }
+
+  if(pin == ""){
+    document.getElementById('editPinError').textContent = "* pin is needed"
+  }
+
+  // validation text event
+
+  document.getElementById('empEdit').addEventListener('input' , (event) =>{
+    inputId = event.target.id;
+    const errorId = `${inputId}Error`;
+    console.log("error id is ", errorId);
+    document.getElementById(errorId).textContent = "";  
+  })
+
+  // gender validation
+
+  const male = document.getElementById("editMale")
+  const female = document.getElementById("editFemale")
+
+  male.addEventListener("click" , () =>{
+    document.getElementById("editGenderError").textContent = "";
+  })
+
+  female.addEventListener("click" , () =>{
+    document.getElementById("editGenderError").textContent = "";
+  })
+
+  return isValid;
 }
 
 // posting edited data to json
 
+
 function saveChanges(empid){
     console.log(empid);
+
 
     const salutation = document.getElementById("editSalutation").value;
     const firstName = document.getElementById("editFirstName").value;
@@ -263,12 +602,35 @@ fetch(`http://localhost:3000/employees/${empid}` , {
     })
     .then((data) => {
         console.log("Data posted successfully:" , data);
+
+        // posting image through edit form----------------------------------
+
+        const profileimg = document.getElementById('imge-upload');
+        var imgObject = new FormData();
+        imgObject.append("avatar", profileimg.files[0]);
+        console.log("img added succesfully" , imgObject);
+
+        fetch(`http://localhost:3000/employees/${empid}/avatar`,{
+            method: "POST",
+            body: imgObject,
+        });
+        // tableContents.unshift(newData);
+        console.log(newData);
+        // displayData(currentPage);
+
+    })
+    .then(() =>{
+        swal.fire({
+            icon: "success",
+            title: "ADD EMPLOYEE SUCCESSFULL",
+            showConfirmButton: false,
+            timer: 1500,
+        });
+        // end of img posting ---------------------------------------
     })
     
     Close('empEdit');
 }
-
-
 
 // Deleting Data from Json
 
@@ -298,7 +660,5 @@ function confirmDelete(id){
         console.log("Deleted");
     })
 }
-
-//  view page
 
 
--- view.js
+++ view.js
@@ -2,6 +2,7 @@ const url =new URL(window.location.href);
 const id = url.searchParams.get("id");
 console.log("received id is => " , id);
 
+
 viewEmployee(id);
 // showEmployeeData(id);
 
@@ -27,12 +28,394 @@ function viewEmployee(id){
         document.getElementById('dob').innerHTML = data.dob;
         document.getElementById('age').innerHTML = data.dob;
 
+     
+        // viewEmployeeImage
+
+        const image = document.getElementById('viewEmpImg');
+        image.src = `http://localhost:3000/employees/${id}/avatar`
+    })
+console.log("function add id = " + id);
+}
+// end----------------------------------------------
+
+// edit employee
+//  edit employee Get
+
+// editEmployee(id);
+let editbtn = document.getElementById("editBtn");
+editbtn.addEventListener("click",() =>{
+    editEmployee(id);
+}) 
+    
+
+function editEmployee(id){
+
+    console.log("employee id = " + id);
+ 
+    let a = document.getElementById('empEdit')
+    a.style.display = "block";
+   let b = document.getElementById('content')
+   b.style.filter = "blur(3px)";
+ 
+ //   fetching data from json and planting it to empEdit
+ 
+ fetch(`http://localhost:3000/employees/${id}` , {
+     method: "GET" ,
+     headers: {
+         "Content-Type": "application/json",
+     },
+ }) //getting all the data in the id
+     .then((response) => response.json())
+     .then((data) => {
+         console.log(data); //displaying all the data
+ 
+         document.getElementById('editSalutation').value = data.salutation;
+         document.getElementById('editFirstName').value = data.firstName;
+         document.getElementById('editLastName').value = data.lastName;
+         document.getElementById('editEmail').value = data.email;
+         document.getElementById('editPhone').value = data.phone;
+         document.getElementById('editUserName').value = data.username;
+         document.getElementById('editPassword').value = data.password; 
+         document.getElementById('editAddress').value = data.address;
+         document.getElementById('editQualification').value = data.qualifications;
+         document.getElementById('editCountry').value = data.country;
+         document.getElementById('editState').value = data.state;
+         document.getElementById('editCity').value = data.city;
+         document.getElementById('editPin').value = data.pin;
+         
+         // dob change
+ 
+         
+        const [day , month , year ] = data.dob.split("-");
+        const newDob = `${year}-${month}-${day}`;
+        document.getElementById('editDob').value = newDob;
+ 
+         // gender  
+ 
+         document.querySelector(`input[name='editGender'][value='${data.gender}']`).checked = true;
+
+
+     })
+
+    //  image preview in edit employee form
+
+    const editpreview = document.getElementById('viewEditEmpImg')
+    editpreview.src = `http://localhost:3000/employees/${id}/avatar`;
+ 
+     let editsubmit = document.getElementById("saveEdit");
+     editsubmit.addEventListener("click",() =>{
+         saveChanges(id);
+     }) 
+ }
+ 
+ function avatarPreviewView(){
+    const preview = document.getElementById("viewEditEmpImg");
+    preview.src = URL.createObjectURL(event.target.files[0]);
+
+}
+ // posting edited data to json
+ 
+ function saveChanges(id){
+     console.log(id);
+
+     if (
+        salutationValidation("editSalutation") &&
+        firstNameValidation('editFirstName') &&
+        lastNameValidation('editLastName') &&
+        emailValidation('editEmail') &&
+        dobValidation('editDob') &&
+        phoneValidation('editPhone') &&
+        genderValidation() &&
+        addressValidation('editAddress') &&
+        countryValidation('editCountry') &&
+        stateValidation('editState') &&
+        cityValidation('editCity') &&
+        pinValidation('editPin') &&
+        usernameValidation('editUserName') &&
+        passwordValidation('editPassword') &&
+        qualificationsValidation('editQualification')
+      ) { 
+ 
+     const salutation = document.getElementById("editSalutation").value;
+     const firstName = document.getElementById("editFirstName").value;
+     const lastName = document.getElementById("editLastName").value;
+     const email = document.getElementById("editEmail").value;
+     const dob = document.getElementById("editDob").value;
+     const phone = document.getElementById("editPhone").value;
+     const gender = document.querySelector('input[name="gender"]:checked').value;
+     const address = document.getElementById("editAddress").value;
+     const country = document.getElementById("editCountry").value;
+     const state = document.getElementById("editState").value;
+     const city = document.getElementById("editCity").value;
+     const pin = document.getElementById("editPin").value;
+     const username = document.getElementById("editUserName").value;
+     const password = document.getElementById("editPassword").value;
+     const qualifications = document.getElementById("editQualification").value;
+ 
+     const originalDateString = dob;
+      // Parse the original date string
+      let parts = originalDateString.split("-");
+      let year = parts[0];
+      let month = parts[1];
+      let day = parts[2];
+      // Construct the reversed date string
+      let reversedDateString = `${day}-${month}-${year}`;
+      console.log(reversedDateString);
+      const dobb = reversedDateString;
+ 
+ const newData ={
+     salutation,
+     firstName,
+     lastName,
+     email,
+     phone,
+     dob:dobb,
+     gender,
+     qualifications,
+     address,
+     city,
+     state,
+     pin,
+     country,
+     username,
+     password
+ }
+ 
+ console.log("new data fetched" + newData);
+ 
+ fetch(`http://localhost:3000/employees/${id}` , {
+         method: "PUT" ,
+         headers: {
+             "Content-Type" : "application/json",
+         },
+         body:JSON.stringify(newData)
+     })
+     .then((response) => {
+         return response.json();
+     })
+     .then((data) => {
+         console.log("Data posted successfully:" , data);
+
+        //  posting image to json ----------------------------------------
+
+        const profileImgEditView = document.getElementById('viewEditEmp');
+        var imgObjectView = new FormData();
+        imgObjectView.append("avatar", profileImgEditView.files[0]);
+        console.log("img added succesfully" , imgObjectView);
+
+        fetch(`http://localhost:3000/employees/${id}/avatar`,{
+            method: "POST",
+            body: imgObjectView,
+        });
+
+        console.log(newData);
+
+    })
+    .then(() =>{
+        swal.fire({
+            icon: "success",
+            title: "ADD EMPLOYEE SUCCESSFULL",
+            showConfirmButton: false,
+            timer: 1500,
+        });
+
+     })
+     Close('empEdit');
+     
+    //  window.location.href = `view.html?${id}`; 
+    // location.reload(true);
+     
+  }
+}
+
+
+//   employee delete
+
+
+let empDeletee = document.getElementById("deleteBtn");
+empDeletee.addEventListener("click",() =>{
+    
+    let a = document.getElementById('empDelete')
+    a.style.display = "block";
+    let b = document.getElementById('content')
+    b.style.filter = "blur(3px)";
+
+}) 
+
+let deleteEmployee = document.getElementById("deleteEmployee");
+deleteEmployee.addEventListener("click",() =>{
+    
+    console.log("employee id = " + id);
+ 
+   
+
+    fetch(`http://localhost:3000/employees/${id}` , {
+    method: "DELETE" ,
+    headers: {
+        'Content-Type' : 'application/json',
+    }
+    })
+    .then((response) => {
+        return response.json;
+    })
+    .then((data) => {
+        console.log("Deleted");
     })
 
+    window.location.href = "index.html";
+})
 
-console.log("function id = " + id);
+
+
+// closing alll
+
+function Close(id) {
+    let idd = id;
+    document.getElementById(idd).style.display = "none";
+    document.getElementById('content').style.filter = "blur(0px)";
 }
 
 
 
+// validation 
 
+
+function salutationValidation(id) {
+    const salutation = document.getElementById(id).value;
+    if (salutation === "" || salutation === "select") {
+      alert("Salutation is required");
+    //   document.getElementById('errorSalutation').innerText = "Salutation is required"
+      return false;
+    }
+    return true;
+  }
+  
+  function firstNameValidation(id) {
+    const firstName = document.getElementById(id).value;
+    if (firstName === "") {
+      alert("First Name is required");
+      return false;
+    }
+    return true;
+  }
+  
+  function lastNameValidation(id) {
+    const lastName = document.getElementById(id).value;
+    if (lastName === "") {
+      alert("Last Name is required");
+      return false;
+    }
+    return true;
+  }
+  
+  function emailValidation(id) {
+    const email = document.getElementById(id).value;
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!re.test(String(email).toLowerCase())) {
+      alert("Invalid email address");
+      return false;
+    }
+    return true;
+  }
+  
+  function dobValidation(id) {
+    const dob = document.getElementById(id).value;
+    if (dob === "") {
+      alert("Date of Birth is required");
+      return false;
+    }
+    return true;
+  }
+  
+  function phoneValidation(id) {
+    const phone = document.getElementById(id).value;
+    const re = /^\d{10}$/;
+    if (!re.test(phone)) {
+      alert("phone must be 10 digits");
+      return false;
+    }
+    return true;
+
+    
+  }
+  
+  function genderValidation() {
+    const gender = document.querySelector('input[name="gender"]:checked');
+    if (!gender) {
+      alert("Gender is required");
+      return false;
+    }
+    return true;
+  }
+  
+  function addressValidation(id) {
+    const address = document.getElementById(id).value;
+    if (address === "") {
+      alert("Address is required");
+      return false;
+    }
+    return true;
+  }
+  
+  function countryValidation(id) {
+    const country = document.getElementById(id).value;
+    if (country === "" || country === "select") {
+      alert("Country is required");
+      return false;
+    }
+    return true;
+  }
+  
+  function stateValidation(id) {
+    const state = document.getElementById(id).value;
+    if (state === "" || state === "select") {
+      alert("State is required");
+      return false;
+    }
+    return true;
+  }
+  
+  function cityValidation(id) {
+    const city = document.getElementById(id).value;
+    if (city === "") {
+      alert("City is required");
+      return false;
+    }
+    return true;
+  }
+  
+  function pinValidation(id) {
+    const pin = document.getElementById(id).value;
+    if (pin === "") {
+      alert("Pin is required");
+      return false;
+    }
+    return true;
+  }
+  
+  function usernameValidation(id) {
+    const username = document.getElementById(id).value;
+    if (username === "") {
+      alert("Username is required");
+      return false;
+    }
+    return true;
+  }
+  
+  function passwordValidation(id) {
+    const password = document.getElementById(id).value;
+    if (password === "") {
+      alert("Password is required");
+      return false;
+    }
+    return true;
+  }
+  
+  function qualificationsValidation(id) {
+    const qualifications = document.getElementById(id).value;
+    if (qualifications === "") {
+      alert("Qualifications are required");
+      return false;
+    }
+    return true;
+  }
